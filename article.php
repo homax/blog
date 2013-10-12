0a6c0d34eef4f556e6506f89d3cd1d59c9beff9d
Maxim Kutyrew <homaxweb@mail.ru>
2013-10-12 12:12:30 +0400

    add model_articles.php and const THEME in function template()

--- article.php
+++ article.php
@@ -2,6 +2,7 @@
 	require_once "config.php";
 	require_once MODEL_PATH."startup.php";
 	require_once MODEL_PATH."model.php";
+	require_once MODEL_PATH."model_articles.php";
 	startup();
 
 if(isset($_GET['id'])) $id = clrInt($_GET['id']);
@@ -11,6 +12,6 @@ if($id)
 else
 	die("Не указано какую статью выводить");
 
-echo template(THEME_PATH."header.html.php", array('title' => $article['title']));
-echo template(THEME_PATH."article.html.php", array('article' => $article));
-echo template(THEME_PATH."footer.html.php");
+echo template("header.html.php", array('title' => $article['title']));
+echo template("article.html.php", array('article' => $article));
+echo template("footer.html.php");
